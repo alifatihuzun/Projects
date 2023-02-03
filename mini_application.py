b16b7da6e8a753f26b5aaac73d0bbc1670262d2d
alifatihuzun <109788254+alifatihuzun@users.noreply.github.com>
2023-02-04 01:23:44 +0300

    Create mini_application.py

---
+++ mini_application.py
@@ -0,0 +1,11 @@
+upper_limit = 50000
+
+store_name = input("Please enter your store name\n")
+income = int(input("Please enter your income\n"))
+
+if income > upper_limit:
+    print("Congratulations!! "+store_name)
+elif income < upper_limit:
+    print("Alert!! Çok düşük gelir "+str(income))
+else:
+    print("Go on!!")
