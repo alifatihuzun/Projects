9f7b47996bc126888d25701b16d75b8e9941ceac
alifatihuzun <109788254+alifatihuzun@users.noreply.github.com>
2023-02-13 00:37:25 +0300

    Update mini_application.py

--- mini_application.py
+++ mini_application.py
@@ -6,6 +6,6 @@ income = int(input("Please enter your income\n"))
 if income > upper_limit:
     print("Congratulations!! "+store_name)
 elif income < upper_limit:
-    print("Alert!! Çok düşük gelir "+str(income))
+    print("Alert!! Low Income "+str(income))
 else:
     print("Go on!!")
